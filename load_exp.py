4548e9e387389198cf9553e18a771427ae8db562
clarkwkw <clarkwkw@yahoo.com.hk>
2017-12-30 11:17:30 +0800

    cleaner repo structure

--- load_exp.py
+++ load_exp.py
@@ -4,14 +4,14 @@ import models
 import importlib
 
 def parse_args():
-	parser = argparse.ArgumentParser(description="Loads a testing script under experiements/")
+	parser = argparse.ArgumentParser(description="Loads a testing script under experiments/")
 	parser.add_argument("script", type = str, help = "The name of the script")
 	args = parser.parse_args()
 	return args
 
 try:
 	args = parse_args()
-	module = importlib.import_module('experiements.'+args.script)
+	module = importlib.import_module('experiments.'+args.script)
 except ImportError:
 	print("> Cannot load script '%s', abort."%args.script)
 	exit(-1)
